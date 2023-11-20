1fd2cac3fddd962928ecd561d1bf43e25dbaf718
1-cwc <2021694476@qq.com>
2023-11-20 10:05:03 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useCounterStore } from '@/stores/modules/count'
+
+const counter = useCounterStore()
+const increment = () => {
+  counter.count++
+}
+</script>
+<template>
+  <view>Current Count: {{ counter.count }}</view>
+  <button @click="increment">asds</button>
+</template>
